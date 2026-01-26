be3d54676da69f9c93d01c10556569d97f735c00
Calvin <ApexCalvin@gmail.com>
2026-01-25 20:42:46 -0500

    added demo files .html and .java for practicing

---
+++ src/main/java/org/demo/lesson/Demo.java
@@ -0,0 +1,20 @@
+package org.demo.lesson;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Demo {
+    
+    public static void main(String[] args) {
+
+        String zdt = ZonedDateTime.now()
+                        .format(DateTimeFormatter.ofPattern("MMMM d, yyyy h:mma z", Locale.ENGLISH));
+        System.out.println("Zoned: " + zdt);
+
+        String ldt = LocalDateTime.now()
+                        .format(DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss"));
+        System.out.println("Local: " + ldt);
+    }
+}
